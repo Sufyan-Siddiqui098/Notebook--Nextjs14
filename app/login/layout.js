84e63da9437b3897b6d38628f4632f372df039f0
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-04-26 17:11:44 +0500

    Login page created

---
+++ app/login/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from "./page"
+
+export const metadata = {
+    title: "Notebook | Login",
+    description: "Cloud notebook - save your notes on the cloud",
+  };
+
+const layout = () => {
+  return (
+    <Login />
+  )
+}
+
+export default layout
