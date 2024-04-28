0ff6b2ca49c803d1a75e8b96322d2501354012e9
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-04-28 14:53:23 +0500

    Home layout

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,23 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Notebook",
+  title: "Notebook | Home",
   description: "Cloud notebook - save your notes on the cloud",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ToastContainer />
+        {children}
+        </body>
     </html>
   );
 }
