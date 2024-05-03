5e2b3c3a502ddfb654a54afdcee80b05af170243
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-05-03 16:13:59 +0500

    handling route protection

---
+++ middleware.js
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+    
+        const path = request.nextUrl.pathname;
+        const isPublic = path==='/login' || path==="/register";
+        const token = request.cookies.get("token") || "";
+        // console.log('token ', token)
+        if(isPublic && token){
+            return NextResponse.redirect(new URL('/', request.nextUrl))
+        } 
+        if(!isPublic && !token){
+            return NextResponse.redirect(new URL('/login', request.nextUrl))
+        }
+    
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/',
+    '/profile',
+    '/register',
+    '/login',
+    '/register'
+  ],
+}
