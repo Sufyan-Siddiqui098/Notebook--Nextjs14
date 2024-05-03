982fc49eab0c42dd4a13f27df9e59341df96f297
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-05-03 23:59:47 +0500

    home page divided into components

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import AddNoteForm from "@/components/AddNoteForm";
+import Notes from "@/components/Notes";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
+      {/* <Header /> */}
+      <main className="flex min-h-screen flex-col items-center justify- py-24 px-2 sm:px-12">
+        {/* Add a note form */}
+        <AddNoteForm />
 
-    </main>
+        {/* Notes - if presnt */}
+        <Notes />
+      </main>
+    </>
   );
 }
