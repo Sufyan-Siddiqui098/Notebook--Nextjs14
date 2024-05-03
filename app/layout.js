6be12cec517bf0d933994a3dbd9f41f52f6f3d27
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-05-03 23:53:41 +0500

    passing firstname prop to header

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Header from "@/components/Header";
+import { cookies } from "next/headers";
+import { decodeToken } from "./lib/decodeUser";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,10 +15,16 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const token = cookies().get("token")?.value;
+  let firstname ;
+  if(token){
+    firstname = decodeToken(token).firstname;
+  }
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ToastContainer />
+        <ToastContainer />
+        <Header token={token} firstname={firstname} />
         {children}
         </body>
     </html>
