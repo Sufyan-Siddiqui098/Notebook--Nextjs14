0896e487c1b9e7882d3521101d501b01b9a2260a
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-05-03 23:58:58 +0500

    login connected to backend

--- app/login/page.js
+++ app/login/page.js
@@ -1,49 +1,82 @@
-'use client'
-import Link from 'next/link'
-import React, { useState } from 'react'
-
-
+"use client";
+import { loginUser } from "@/app/lib/auth/auth";
+import Link from "next/link";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import SubmitButton from "@/components/SubmitButton";
 
 const LoginPage = () => {
-    const [credentials, setCredentials] = useState({
-        email: "", 
-        password: ""
-    })  
-    return(
-    <div className='flex min-h-screen flex-col items-center justify-center py-24 px-3'>
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
 
-        <form className='flex flex-col justify-center items-center gap-2 py-4 w-full max-w-md'>
-            <h2 className='relative text-white text-2xl font-semibold my-2 mb-8 before:content-[""] before:w-7 before:h-[2.6px] before:bg-[#63c34e] before:absolute before:top-[110%] before:left-0 before:right-0 before:mx-auto before:my-1 before:rounded'>Login</h2>
+  const [pending, setPending] = useState(false);
 
-            <div className='flex flex-col gap-2 w-full px-2 text-black sm:px-4 '>
-            <input 
-                onChange={(e)=>setCredentials({...credentials, "email":e.target.value})}
-                value={credentials.email} 
-                className='rounded w-full py-[12px] px-2 text-base' 
-                type="email" 
-                required 
-                placeholder='Email' 
-            />
-            <input 
-                onChange={(e)=>setCredentials({...credentials, "password":e.target.value})}
-                value={credentials.password} 
-                className='rounded w-full py-[12px] px-2 text-base' 
-                type="password" 
-                required 
-                placeholder='Password' 
-            />
-            </div>
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      setPending(true);
+      const response = await loginUser(credentials);
+      console.log("resp", response);
+      if (response.success) {
+        toast.success(response.message);
+        // for now this is how we replace the location
+        window.location.replace("/");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setPending(false);
+    }
+  };
 
-            <div className='flex flex-col sm:flex-row items-center justify-center text-sm gap-[2px] sm:gap-1 mt-3 px-2'>
-                <Link className='hover:underline mr-1' href="/forgot-password">Forgot Your Password ?</Link>
-                <Link className=' font-semibold text-[#63c34e]' href="/register">Sign up</Link>
-            </div>
-            <button className='border my-3 mt-5 p-[8px_20px] bg-transparent text-white border-white rounded hover:bg-[#63c34e] hover:text-black hover:border-transparent focus:border-transparent focus:bg-[#63c34e] focus:text-black transition' >Login</button>
-        </form>
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-24 px-3">
+      <form
+        onSubmit={onSubmitHandler}
+        className="flex flex-col justify-center items-center gap-2 py-4 w-full max-w-md"
+      >
+        <h2 className='relative text-white text-2xl font-semibold my-2 mb-8 before:content-[""] before:w-7 before:h-[2.6px] before:bg-[#63c34e] before:absolute before:top-[110%] before:left-0 before:right-0 before:mx-auto before:my-1 before:rounded'>
+          Login
+        </h2>
 
+        <div className="flex flex-col gap-2 w-full px-2 text-black sm:px-4 ">
+          <input
+            onChange={(e) =>
+              setCredentials({ ...credentials, email: e.target.value })
+            }
+            value={credentials.email}
+            className="rounded w-full py-[12px] px-2 text-base"
+            type="email"
+            required
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) =>
+              setCredentials({ ...credentials, password: e.target.value })
+            }
+            value={credentials.password}
+            className="rounded w-full py-[12px] px-2 text-base"
+            type="password"
+            required
+            placeholder="Password"
+          />
+        </div>
 
+        <div className="flex flex-col sm:flex-row items-center justify-center text-sm gap-[2px] sm:gap-1 mt-3 px-2">
+          <Link className="hover:underline mr-1" href="/forgot-password">
+            Forgot Your Password ?
+          </Link>
+          <Link className=" font-semibold text-[#63c34e]" href="/register">
+            Sign up
+          </Link>
+        </div>
+
+        <SubmitButton text={"login"} pending={pending} />
+      </form>
     </div>
-)
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
