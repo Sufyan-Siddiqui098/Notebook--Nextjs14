934a26e8698a19f3013c91e7c1d6562f342ad472
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-05-07 23:12:50 +0500

    forgotPassword api call added

--- app/lib/auth/auth.js
+++ app/lib/auth/auth.js
@@ -1,32 +1,46 @@
 // 'use server'
-import axios from 'axios'
+import axios from "axios";
 
-export const loginUser = async(credentials)=>{
-    try {
-       const {data} = await axios.post('/api/login', credentials);
-        return data;
+export const loginUser = async (credentials) => {
+  try {
+    const { data } = await axios.post("/api/login", credentials);
+    return data;
+  } catch (error) {
+    throw new Error(
+      error.response ? error.response.data.message : error.message
+    );
+  }
+};
 
-    } catch (error) {
-        throw new Error( error.response? error.response.data.message : error.message)
-    }   
-}
+export const regisgerUser = async (credentials) => {
+  try {
+    const { data } = await axios.post("/api/register", credentials);
+    return data;
+  } catch (error) {
+    throw new Error(
+      error.response ? error.response.data.message : error.message
+    );
+  }
+};
 
-export const regisgerUser = async(credentials)=>{
-    try {
-        const {data} = await axios.post("/api/register", credentials);
-        return data;
-    } catch (error) {
-        throw new Error( error.response? error.response.data.message : error.message)
-    }
-}
+export const logoutUser = async () => {
+  try {
+    const { data } = await axios.get("/api/logout");
+    return data;
+  } catch (error) {
+    throw new Error(
+      error.response ? error.response.data.message : error.message
+    );
+  }
+};
 
-export const logoutUser = async()=>{
-    try {
-        const {data} = await axios.get('/api/logout');
-        console.log("Logout response ", data)
-        return data;
-    } catch (error) {
-        // return {success: false, message: error.message}
-        throw new Error( error.response? error.response.data.message : error.message)
-    }
-}
+export const forgotPassword = async (credentials) => {
+  try {
+    const { data } = await axios.post("/api/forget-password");
+    return data;
+  } catch (error) {
+    throw new Error(
+      error.response ? error.response.data.message : error.message
+    );
+  }
+};
